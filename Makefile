2706d919f3c80d763990e5fdf12fe439872726a0
Anchit Bajaj <ab@abifog.com>
2021-02-17 23:23:54 +0530

    Use goose for migrations

--- Makefile
+++ Makefile
@@ -7,3 +7,15 @@ build:
 # required to build with older glibc
 container:
 	podman run -it --rm -v $(shell pwd):/src:z docker.io/library/golang:1.14.15-stretch
+
+# USAGE: make migrate_new MNAME=whatever_name
+migrate_new:
+	goose -dir ./db/sql sqlite3 ./betterbin.sqlite create $(MNAME) sql
+
+# up all migrations
+migrate_up:
+	goose -dir ./db/sql sqlite3 ./betterbin.sqlite up
+
+# down one migration
+migrate_down:
+	goose -dir ./db/sql sqlite3 ./betterbin.sqlite down
--- db/sql/20210217231700_init.sql
+++ db/sql/20210217231700_init.sql
@@ -1,3 +1,5 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS "pastes" (
 	"id"	TEXT,
 	"title"	TEXT,
@@ -20,9 +22,14 @@ CREATE TABLE IF NOT EXISTS "file_drops" (
 	"id"	TEXT,
 	"title"	TEXT,
 	"filename"	TEXT NOT NULL,
-	"password"	TEXT,
-	"expiry"	TEXT NOT NULL,
 	"created"	TEXT NOT NULL,
-	"burn"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("id")
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS "pastes";
+DROP TABLE IF EXISTS "file_drops";
+DROP TABLE IF EXISTS "shortened_links";
+-- +goose StatementEnd
